dc1a9f8507395c15f9c21c4e4f943415ad86413c
pashatay <pashabonilla@gmail.com>
2024-01-15 16:52:23 -0500

    added a file remover

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from anonymizer import anonymize_file
 import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
 UPLOAD_FOLDER = "./uploads"
 ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
@@ -21,29 +21,36 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'message': 'No file part in the request'}), 400
+    try:
+        if 'file' not in request.files:
+            return jsonify({'message': 'No file part in the request'}), 400
 
-    file = request.files['file']
+        file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'message': 'No file selected for uploading'}), 400
+        if file.filename == '':
+            return jsonify({'message': 'No file selected for uploading'}), 400
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        input_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        output_file_path = os.path.join(app.config['UPLOAD_FOLDER'], "anonymized_" + filename)
-        config_path = os.path.join(CONFIG_FILE)  # Path to the config file
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            input_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            output_file_path = os.path.join(app.config['UPLOAD_FOLDER'], "anonymized_" + filename)
+            config_path = os.path.join(CONFIG_FILE)
 
-        file.save(input_file_path)
+            file.save(input_file_path)
 
-        try:
-            anonymized_file_path = anonymize_file(input_file_path, output_file_path, config_path)
-            return send_file(anonymized_file_path, as_attachment=True)
-        except Exception as e:
-            return jsonify({'message': str(e)}), 500
-    else:
-        return jsonify({'message': 'Unsupported file format'}), 400
+            anonymize_file(input_file_path, output_file_path, config_path)
+
+            response = send_file(output_file_path, as_attachment=True)
+
+            # Clean up: Remove uploaded and cleaned files
+            os.remove(input_file_path)
+            os.remove(output_file_path)
+
+            return response
+        else:
+            return jsonify({'message': 'Unsupported file format'}), 400
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
